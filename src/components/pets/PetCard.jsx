869b025cc91c34d53dddbeaaea183914caf619f3
darynazin <danayu@ukr.net>
2025-02-21 15:39:46 +0100

    refined styles

--- src/components/pets/PetCard.jsx
+++ src/components/pets/PetCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function PetCard({ pet }) {
+  const navigate = useNavigate();
+
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    <div className="card card-compact bg-base-100 shadow-xl">
       <figure className="h-64">
         <img
           src={pet.image}
@@ -12,16 +15,21 @@ function PetCard({ pet }) {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{pet.name}</h2>
-        <div className="text-sm">
-          <p>
+        <div className="flex items-center text-sm w-full">
+          <p className="mr-4">
             <span className="font-semibold">Species:</span> {pet.species}
           </p>
-          <p>
+          <p className="mr-4">
             <span className="font-semibold">Age:</span> {pet.age} years
           </p>
-        </div>
-        <div className="card-actions justify-end mt-4">
-          <button className="btn btn-sm btn-outline">✏️ Edit</button>
+          <div className="ml-auto">
+            <button
+              className="btn btn-sm btn-outline"
+              onClick={() => navigate(`/pets/${pet._id}`)}
+            >
+              ✏️ Edit
+            </button>
+          </div>
         </div>
       </div>
     </div>
