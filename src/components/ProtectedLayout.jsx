9c6c3494f8432691cb19f4d0da6fc7ef8a9b3b35
darynazin <danayu@ukr.net>
2025-03-11 18:40:02 +0100

    protected layout uncomment

--- src/components/ProtectedLayout.jsx
+++ src/components/ProtectedLayout.jsx
@@ -1,33 +1,33 @@
-// import React, { useEffect, useState } from "react";
-// import { Navigate, Outlet } from "react-router-dom";
-// import { getSession } from "../services/api";
+import React, { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { getSession } from "../services/api";
 
-// function ProtectedLayout({ allowedRoles }) {
-//   const [session, setSession] = useState(null);
-//   const [loading, setLoading] = useState(true);
+function ProtectedLayout({ allowedRoles }) {
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-//   useEffect(() => {
-//     const fetchSession = async () => {
-//       try {
-//         const response = await getSession();
-//         setSession(response.data);
-//       } catch (error) {
-//         console.error("Error fetching session:", error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
+  useEffect(() => {
+    const fetchSession = async () => {
+      try {
+        const response = await getSession();
+        setSession(response.data);
+      } catch (error) {
+        console.error("Error fetching session:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-//     fetchSession();
-//   }, []);
+    fetchSession();
+  }, []);
 
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-//   const isAuthenticated = session && session.user && allowedRoles.includes(session.user.role);
-//   console.log(session.user)
-//   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-// }
+  const isAuthenticated = session && session.user && allowedRoles.includes(session.user.role);
+  console.log(session.user)
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+}
 
-// export default ProtectedLayout;
+export default ProtectedLayout;
